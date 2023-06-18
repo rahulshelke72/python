053ef331dfff9bf2db5ca04d6ff7fe7c41da9c14
rahul4172 <rahulshelke4172@gmail.com>
2023-06-18 23:38:36 +0530

    user input , control statements

---
+++ Control Statements/Control_Statements.py
@@ -0,0 +1,13 @@
+condition = False
+name = "sam"
+
+if condition == True:
+    print("The condition ")
+    print("was true")
+elif name == "Roger":
+    print("Hello Roger")
+elif name == "Flavio":
+    print("Hello Flavio")
+else:
+    print("The condition")
+    print("was false")
