5105a837e512eaa72223655c28449853c8f3e750
Rahul <rahulshelke4172@gmail.com>
2023-08-27 23:18:33 +0530

    Sort in List

--- Lists/Lists.py
+++ Lists/Lists.py
@@ -3,5 +3,13 @@ dogs = ["Roger" ,1, "Syd",True]
 print("Beau" in dogs)
 
 dogs[2] = "Beau"
+dogs.append(["Judah",5])
+dogs += ["Akash", 4]
 print(dogs)
 print(dogs[-1])
+
+dogs.remove("Quincy")
+
+print(dogs.pop())
+print(dogs)
+
---
+++ Lists/Lists2.py
@@ -0,0 +1,8 @@
+items = ["Roger", "Syd","beau" , "Quincy"]
+
+items[1:1] = ["Test1"]
+
+items.sort(key=str.lower)
+print(items)
+
+
