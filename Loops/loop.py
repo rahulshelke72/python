df49f67beef11df0f2a5c6d371ae0093da0c526f
Rahul <rahulshelke4172@gmail.com>
2023-09-08 23:24:41 +0530

    Objects , Loops , Closures

---
+++ Loops/loop.py
@@ -0,0 +1,22 @@
+# Loops 
+
+condition = True
+while condition == True:
+    print("The condition is True")
+    condition = False
+
+count = 0
+while count < 10:
+    print("The condition is True")
+    count += 1
+
+print("after the loop")
+
+
+items = ["beau","syd","quincy"]
+for index , item in enumerate(items):
+    print(index,item)
+
+for item in range(15):
+    print(item)
+
---
+++ Objects/objects.py
@@ -0,0 +1,16 @@
+# Objects 
+
+age = 8
+
+print(age.real)
+print(age.imag)
+print(age.bit_length())
+
+
+items = [1,2]
+items.append(3)
+items.pop()
+print(id(items))
+
+
+
