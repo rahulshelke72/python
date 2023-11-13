70cfd1b3b23148c0d091b43c34033995699fe792
Rahul <rahulshelke4172@gmail.com>
2023-11-13 22:41:43 +0530

    Polymorphism , Operator Overloading

---
+++ Classes/Operator_Overloading.py
@@ -0,0 +1,13 @@
+# Operator Overloading
+
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def __gt__(self, other):
+        return True if self.age > other.age else False
+
+roger = Dog('Roger' , 8)
+syd = Dog('syd' , 9)
+
+print(roger > syd)
