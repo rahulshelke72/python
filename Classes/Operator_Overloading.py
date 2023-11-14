e243520309c03d96cd3fcf0d1a1199a6b98d684e
Rahul <rahulshelke4172@gmail.com>
2023-11-14 22:33:44 +0530

    black jack

--- Classes/Operator_Overloading.py
+++ Classes/Operator_Overloading.py
@@ -11,3 +11,16 @@ roger = Dog('Roger' , 8)
 syd = Dog('syd' , 9)
 
 print(roger > syd)
+
+# __add__() respond to the + operator
+# __sub__() respond to the - operator
+# __mul__() respond to the * operator
+# __truediv__() respond to the / operator
+# __floordiv__() respond to the // operator
+# __mod__() respond to the % operator
+# __pow__() respond to the ** operator
+# __rshift__() respond to the >> operator
+# __lshift__() respond to the << operator
+# __and__() respond to the & operator
+# __or__() respond to the | operator
+# __xor__() respond to the ^ operator
